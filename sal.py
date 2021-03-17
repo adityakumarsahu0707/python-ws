329836b10180b014935e3a26a1e0c1671560f9cb
root <adityakumarsahu45@gmail.com>
2021-03-17 10:14:53 +0530

    commit from rhel

---
+++ sal.py
@@ -0,0 +1,14 @@
+class employee:
+    def __init__(self, name):
+        self.name = name
+
+    def getName(self):
+        print("Your Name : ", self.name)
+
+class salariedEmployee():
+
+
+
+
+emp1=employee("Aditya")
+emp1.getName()
