f3b8753e8db50e80ea28c896ee902719341acdf7
root <adityakumarsahu45@gmail.com>
2021-03-17 10:29:48 +0530

    change add getId

--- sal.py
+++ sal.py
@@ -5,10 +5,17 @@ class employee:
     def getName(self):
         print("Your Name : ", self.name)
 
-class salariedEmployee():
+class salariedEmployee(employee):
+    def __init__(self,name):
+        self.name=name
+        super().__init__(name)
+    def getId(self):
+        self.id=input("Enter Your ID = ")
+        return("Your Name = ", self.name , " & Your ID = ", self.id)
 
 
 
 
 emp1=employee("Aditya")
 emp1.getName()
+emp1=emp.getID()
